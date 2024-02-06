bbb080c56585f976e13e05f00c1eafb383fd7f7f
Wael Elkadim <wael.elkadim@microsoft.com>
2024-02-06 11:36:06 +0400

    Add roles sample

--- Application.Web/Pages/Privacy.cshtml.cs
+++ Application.Web/Pages/Privacy.cshtml.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Application.Web.Pages
 {
+    [Authorize(Roles = "S-1-2-0000")]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
